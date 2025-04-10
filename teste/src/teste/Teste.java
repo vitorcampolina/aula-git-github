01c23f8ebf5d1cb98a0204fc009f3782b3c174e7
Vitor Campolina <103151856+vitorcampolina@users.noreply.github.com>
2025-04-10 13:58:45 -0300

    Update Teste.java

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -1,10 +1,9 @@
 package teste;
+	public class Teste {
 
-public class Teste {
+		public static void main(String[] args) {
+			int num1 = 3, num2 = 5, soma;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			soma = num1 + num2;
 	}
-
 }
